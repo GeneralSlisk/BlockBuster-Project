a9764adff3cf9d0ba1da3de6d2b7be9cc226c8b2
Daniel <team.delta@hotmail.com>
2022-09-08 22:18:05 +0200

    Project

---
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+	public float moveSpeed;
+	public GameObject target;
+
+	private Transform rigTransform;
+
+	// Use this for initialization
+	void Start() {rigTransform = this.transform.parent;
+		
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(target == null){
+			return;
+		}
+
+		rigTransform.position = Vector3.Lerp(rigTransform.position, target.transform.position, 
+			Time.deltaTime * moveSpeed);
+	}
+
+}
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Player : MonoBehaviour {
+
+  public int health;
+  public event Action<Player> onPlayerDeath;
+
+  void OnCollisionEnter (Collision col) {
+      Enemy enemy = col.collider.gameObject.GetComponent<Enemy>();
+      if(enemy) {
+        collidedWithEnemy(enemy);
+      }
+  }
+
+  void collidedWithEnemy(Enemy enemy) {
+    enemy.Attack(this);//this Destroy(this.gameObject);
+    if(health <= 0) {
+      if(onPlayerDeath != null) {
+        onPlayerDeath(this);//this
+      }
+    }
+  }
+}
